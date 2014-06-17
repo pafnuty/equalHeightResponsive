8bbbd647371d05e93bb96bf8e6fe8db2e4c8ad13
Pavel Belousov <pafnuty10@gmail.com>
2014-06-17 13:10:28 +0400

    v.1.2
    - Плагин переписан по принятым в сообществе правилам написания плагинов,
    что дало так же небольшую прибавку в скорости работы.
    - Добавлена возможность устанавливать свойство line-height для
    элементов.
    - Добавлен более правильный способ ручной переинициализации плагина.

--- jquery.equalheightresponsive.js
+++ jquery.equalheightresponsive.js
@@ -1,7 +1,8 @@
 /**
  * Плагин     : jQuery.equalHeightResponsive
- * Версия     : 1.1 (28.04.2014)
- * Автор      : ПафНутиЙ 
+ * Версия     : 1.2 (17.06.2014)
+ * Репозиторий: http://git.io/OwM7Ow
+ * Автор      : ПафНутиЙ
  * Twitter    : @pafnuty_name
  * Назначение : Автоматическое выравнивание высоты блоков (в каждом ряду своя высота) для адаптивных сайтов. Плагин срабатывает при событиях window:
  * load
@@ -10,8 +11,9 @@
  *
  * Плагин основан на коде из поста: http://css-tricks.com/equal-height-blocks-in-rows/
  *
+ *
  * Использование:
- * HTML: 
+ * HTML:
  * <div class="container">
  *     <div class="element"></div>
  *     <div class="element"></div>
@@ -24,62 +26,94 @@
  * });
  *
  * Для повторной инициализации делаем так:
+ * $('.element').equalHeightResponsive('refresh');
+ * или
+ * $('.element').trigger('equal-refresh.equalHeightResponsive');
+ * или
  * $('.element').trigger('equal-refresh');
- * 
+ *
+ * Так же возможно устанавливать обрабатываемым блокам свойство line-height, равное (высота - 4px),
+ * для этого нужно "активировать" опцию setLineHeight, вот так:
+ * $('.element').equalHeightResponsive({setLineHeight : true});
+ *
+ *
  */
 
-(function ($) {
-	/**
-	 * Основная функция скрипта
-	 * Подставляет высоту самого высокого элемента
-	 * всем остальным элементам, чья позиция (ряд)
-	 * является такой же, что и у текущего. Т.е. у каждого ряда будет своя высота.
-	 */
-	$.fn.equalheightRun = function () {
-		var maxHeight = 0,
-			currentRowStart = 0,
-			rowEls = new Array(),
-			$el,
-			topPosition = 0;
-		$.each(this, function () {
 
-			$el = $(this);
-			$($el).height('auto')
-			topPostion = $el.offset().top;
+;(function ($, window, document, undefined) {
 
-			if (currentRowStart != topPostion) {
-				for (currentEl = 0; currentEl < rowEls.length; currentEl++) {
-					rowEls[currentEl].height(maxHeight);
-				}
-				rowEls.length = 0; // empty the array
-				currentRowStart = topPostion;
-				maxHeight = $el.height();
-				rowEls.push($el);
+	var maxHeight = 0,
+		currentRowStart = 0,
+		rowEls = new Array(),
+		$el,
+		topPosition = 0,
+		defaults = {
+			setLineHeight: false
+		},
+		methods = {
+			init: function (options) {
+
+				var props = $.extend({}, defaults, options),
+					_this = this,
+					_run = function () {
+						methods.run(_this, props);
+					};
+
+				$(window).on('load.equalHeightResponsive resize.equalHeightResponsive equal-refresh.equalHeightResponsive', _this, _run);
+
+				return this;
+
+			},
+			run: function (elements, options) {
+				$.each(elements, function () {
+					$el = $(this);
+					$($el).height('auto')
+					topPostion = $el.offset().top;
+
+					if (currentRowStart != topPostion) {
+						for (currentEl = 0; currentEl < rowEls.length; currentEl++) {
+							rowEls[currentEl].height(maxHeight);
+						}
+						rowEls.length = 0;
+						currentRowStart = topPostion;
+						maxHeight = $el.height();
+						rowEls.push($el);
+					}
+					else {
+						rowEls.push($el);
+						maxHeight = (maxHeight < $el.height()) ? ($el.height()) : (maxHeight);
+					}
+					for (currentEl = 0; currentEl < rowEls.length; currentEl++) {
+						rowEls[currentEl].height(maxHeight);
+						if (options.setLineHeight) {
+							rowEls[currentEl].css({
+								'line-height': maxHeight - 4 + 'px'
+							})
+						};
+					}
+				});
+
+				return this;
+			},
+			refresh: function () {
+				$(window).trigger('equal-refresh.equalHeightResponsive');
+
+				return this;
 			}
-			else {
-				rowEls.push($el);
-				maxHeight = (maxHeight < $el.height()) ? ($el.height()) : (maxHeight);
-			}
-			for (currentEl = 0; currentEl < rowEls.length; currentEl++) {
-				rowEls[currentEl].height(maxHeight);
-			}
-		});
-		return this;
-	}
+		};
 
-	/**
-	 * Функция пересчёта высоты элементов
-	 * Срабатывает при загрузке и ресайзе окна.
-	 */
-	$.fn.equalHeightResponsive = function () {
-		var _this = this;
+	$.fn.equalHeightResponsive = function (method) {
 
-		function equal() {
-			_this.equalheightRun();
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
-		$(window).on('load resize equal-refresh', _this, equal);
+		else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		}
+		else {
+			$.error('Метод с именем ' + method + ' не существует для jQuery.equalHeightResponsive');
+		}
 
-		return this;
-	}
+	};
 
-})(jQuery);
+})(jQuery, window, document);
